0868f3a55c241812694d377e712d9be2e4ffe24f
Daley-T <tivoniadaley123@gmail.com>
2026-09-10 02:05:06 -0400

    task 2: average temperature

--- devices.py
+++ devices.py
@@ -9,7 +9,19 @@ readings = [
 devices = readings.copy()
 
 def list_devices(array):
+    print("Device Name" , " " , "Temperature")
     for i in range(len(array)):
         print(array[i]["name"] , " " ,array[i]["temp"])
 
 list_devices(devices)
+
+
+def avg_temp(array):
+    avg:float = 0.0
+    new_sum:float = 0.0
+    for i in range(len(array)):
+        new_sum = new_sum + devices[i]["temp"]
+        avg = round((new_sum / (len(array))),2)
+    print("\nAverage Temp. = " ,avg)
+
+avg_temp(devices)
